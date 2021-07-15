2517ae8de2cd5b82a71afc5dd0521fb63fdc3532
Curtiss Williams <66583828+curtywill@users.noreply.github.com>
2021-07-15 16:49:20 -0500

    Optimizations

--- maze_generator.py
+++ maze_generator.py
@@ -7,8 +7,8 @@ import random
 
 pygame.init()
 
-WIDTH, HEIGHT = 400, 400
-ROWS, COLS = 20, 20
+WIDTH, HEIGHT = 800, 800
+ROWS, COLS = 40, 40
 CELL_WIDTH = WIDTH // ROWS
 WHITE = (255, 255, 255)
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -16,7 +16,9 @@ pygame.display.set_caption("Wilson's Algorithm")
 
 cells = []
 
-def index(i: int, j: int) -> int:
+def index(nums: tuple) -> int:
+    i = nums[0]
+    j = nums[1]
     if 0 <= i < ROWS and 0 <= j < COLS:
         return ROWS * i + j
     return -1
@@ -41,19 +43,13 @@ class Cell:
         if self.walls[3]:
             pygame.draw.line(WIN, WHITE, (x, y + CELL_WIDTH), (x, y))
 
-    def restore_wall(self) -> None:
-        self.walls[0] = True
-        self.walls[1] = True
-        self.walls[2] = True
-        self.walls[3] = True
-
     def generate_options(self, path: list) -> list:
         options = []
 
-        top = index(self.i - 1, self.j)
-        right = index(self.i, self.j + 1)
-        bottom = index(self.i + 1, self.j)
-        left = index(self.i, self.j - 1)
+        top = index((self.i - 1, self.j))
+        right = index((self.i, self.j + 1))
+        bottom = index((self.i + 1, self.j))
+        left = index((self.i, self.j - 1))
 
         if (top != -1 and top != path[-1]):
             options.append((self.i - 1, self.j))
@@ -85,10 +81,6 @@ def remove_walls(a: Cell, b: Cell) -> None:
         a.walls[0] = False
         b.walls[2] = False
 
-def restore_walls(arr: list) -> None:
-    for c in arr:
-        cells[index(c[0], c[1])].restore_wall()
-
 def main():
     vertices = []
     maze = []
@@ -107,29 +99,23 @@ def main():
         coords = random.choice(vertices)
         path.append(coords)
         while coords not in maze: # perform random walk until we reach cell in maze
-            curr = cells[index(coords[0], coords[1])]
+            curr = cells[index(coords)]
             options = curr.generate_options(path)
             option = random.choice(options)
             if len(options) > 1 and option in path:
                 options.remove(option)
                 option = random.choice(options)
-            remove_walls(curr, cells[index(option[0], option[1])])
             coords = option
-            if coords in path:
-                for i in range(len(path)):
-                    if path[i] == coords:
-                        restore_walls(path[i:])
-                        path = path[:i]
-                        if len(path) != 0:
-                            coords = path[-1]
-                        else:
-                            path.append(coords)
-                        break
-            else:
+            try:
+                i = path.index(coords)
+            except ValueError:
                 path.append(coords)
-        
+            else:
+                path = path[:i+1]
+
         maze.extend(path)
         for i in range(len(path)-1):
+            remove_walls(cells[index(path[i])], cells[index(path[i+1])])
             vertices.remove(path[i])
         
     while len(vertices) != 0:
@@ -137,15 +123,15 @@ def main():
     
     for i in range(ROWS):
         for j in range(COLS):
-            cells[index(i, j)].show()
+            cells[index((i, j))].show()
+    
+    pygame.display.update()
 
-    while True:
-        
+    while True:    
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
 
-        pygame.display.update()
-       
+        
 main()
